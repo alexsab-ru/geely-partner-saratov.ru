c38a56fcd25cfa62bca7346fcf0731714f0f807e
Alexander Sabanov <alexsab@mail.ru>
2024-09-06 12:05:32 +0400

    upd menu

--- src/const.js
+++ src/const.js
@@ -29,10 +29,10 @@ export const LINK_WIDGET = 'https://yandex.ru/map-widget/v1/-/';
 export const LINK_WIDGET_ORGNIZATION = '';
 // Ссылки под хедером
 export const LINKS_MENU = [
-	{url: 'cars/', name: 'Авто в наличии'},
+	// {url: 'cars/', name: 'Авто в наличии'},
 	{url: 'models/', name: 'Модели'},
 	{url: 'special-offers/', name: 'Спецпредложения'},
-	{url: 'news/', name: 'Новости'},
+	// {url: 'news/', name: 'Новости'},
 	{url: 'test-drive/', name: 'Запись на тест-драйв'},
 	{url: 'service-request/', name: 'Запись на сервис'},
 	{url: '#services', name: 'Услуги'},
